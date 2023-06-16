8ccae0df632fd3c41177236e61e0e7bf65ca9874
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2023-06-16 09:34:36 +0200

    update interface

--- client/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ client/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void rbCesar_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
