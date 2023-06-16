51ba07c7e1aed424675a8cf51a539f99942974c1
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2023-06-16 10:09:56 +0200

    update

--- client/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ client/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace WpfApp1
 {
@@ -39,9 +40,39 @@ namespace WpfApp1
             {
                 Console.WriteLine(ex.Message);
             }
-            
         }
 
-        
+        private void txtInputString_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string input = txtInput.Text.ToUpper().Replace(" ", ""); // Convertir en majuscules et supprimer les espaces
+
+                // Vérifier si la chaîne est conforme à l'expression régulière
+                if (Regex.IsMatch(input, @"^[A-Z]+$"))
+                {
+                    // Appeler le service de chiffrement en fonction du radio-bouton sélectionné
+                    if (rbCesar.IsChecked == true)
+                    {
+                        
+                    }
+                    else if (rbVigenere.IsChecked == true)
+                    {
+                        
+                        
+                    }
+                    else if (rbAES.IsChecked == true)
+                    {
+                        
+                    }
+                }
+                else{
+                
+                    // Afficher un message d'erreur si la chaîne ne contient pas que des lettres majuscules
+                    MessageBox.Show("La chaîne doit être composée uniquement de lettres majuscules.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+        }
     }
 }
